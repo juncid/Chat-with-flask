513076af8bf88a6572f81943a0ab5f6af59f4bab
juncid <juncid@gmail.com>
2019-08-25 19:48:38 -0400

    Creacion de funciones iniciales y primer hola mundo en templates.

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vkakajwjeuejcv#'
+socketio = SocketIO(app)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def sessions():
+    return render_template('session.html')
 
+
+def messageReceived(methos=['GET', 'POST']):
+    print('el mensaje fue recibido!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: '+str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
